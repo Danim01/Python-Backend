ae5026c4a6410916c111e68de711786abb720bd0
Danim01 <danibuimon.09@gmail.com>
2024-09-16 21:19:11 -0500

    Second commit

--- app_Project/Productos/views.py
+++ app_Project/Productos/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from Productos import Productos
+from django.http import HttpResponse, JsonResponse
+from Productos.models import Productos
 
 # Create your views here.
 def index(request):
 
-  if request == "GET":
-    products = Productos
-    return HttpResponse("Hola, estas accediendo a la vista de productos")
+  if request.method == "GET":
+    Products = list(Productos.objects.all().values(
+      "id", "name", "precio", "stock"
+    ))
+    return JsonResponse(data = {"Message": "Ok", "Producto": Products})
